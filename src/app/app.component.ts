53c036171542e1d56d19bdb5b8d4e6051880f613
KevinKing66 <kevinvallenato2002@gmail.com>
2024-08-03 23:21:50 -0500

    se comienza a trabajar en los componentes anteriormente creados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { FooterComponent } from '../share/footer/footer.component';
+import { HeaderComponent } from '../share/header/header.component';
+import { ColorTestComponent } from '../share/color-test/color-test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainPageComponent],
+  imports: [RouterOutlet, MainPageComponent, FooterComponent, HeaderComponent, ColorTestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
