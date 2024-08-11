58f39076851ccc5387b3c179e728c94f7c4d60b0
KevinKing66 <kevinvallenato2002@gmail.com>
2024-08-10 20:53:11 -0500

    se soluciona error

--- src/share/about-us/about-us.component.ts
+++ src/share/about-us/about-us.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { TechnologiesComponent } from '../technologies/technologies.component';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-about-us',
